cdd8ddb7283b2c56512d2d6e06abf0ed54c20863
Jonah Bray <110523@TISD-AD.tomballisd.net>
2020-02-25 15:29:42 -0600

    Changing Motor ID's 2&6

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -20,12 +20,12 @@ package frc.robot;
 public final class Constants {
     // CAN/MOTOR DEVICE PORTS
     public static final int MOTOR_LEFT_1_ID = 4;
-    public static final int MOTOR_LEFT_2_ID = 2;
+    public static final int MOTOR_LEFT_2_ID = 6;
     public static final int MOTOR_RIGHT_2_ID = 1;
     public static final int MOTOR_RIGHT_1_ID = 3;
     public static final int MOTOR_ARM_ID = 7;
     public static final int MOTOR_CLIMB_ID = 5;
-    public static final int MOTOR_INTAKE_ID = 6;
+    public static final int MOTOR_INTAKE_ID = 2;
 
     // SENSOR PORTS
     public static final int SWITCH_ARM_UP_ID = 9;
