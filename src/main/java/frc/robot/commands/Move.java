5bf0f6d3bcb7245f9405c110febdb5043bc53f90
AnHonestLawyer <chancestorey@gmail.com>
2020-03-08 14:38:34 -0500

    bunch of binaries?

--- src/main/java/frc/robot/commands/Move.java
+++ src/main/java/frc/robot/commands/Move.java
@@ -7,10 +7,16 @@
 
 package frc.robot.commands;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.DriveTrain;
 
 public class Move extends CommandBase {
+  TalonSRX talon = new TalonSRX(0);
   /**
    * Creates a new Move.
    */
@@ -25,6 +31,10 @@ public class Move extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    //talon.changeControlMode(ControlMode.Position); //Change control mode of talon, default is PercentVbus (-1.0 to 1.0)
+    //talon.setFeedbackDevice(FeedbackDevice.QuadEncoder); //Set the feedback device that is hooked up to the talon
+    //talon.setPID(0.5, 0.001, 0.0); //Set the PID constants (p, i, d)
+    //talon.enableControl(); //Enable PID control on the talon
   }
 
   // Called every time the scheduler runs while the command is scheduled.
